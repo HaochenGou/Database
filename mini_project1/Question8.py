decfcf1a787526e263eff9ce138d9e62dda0477a
Zifeiyu <44186320+Zifeiyu2000@users.noreply.github.com>
2019-11-03 14:17:52 -0700

    Add files via upload

---
+++ mini_project1/Question8.py
@@ -0,0 +1,54 @@
+import sqlite3
+import random
+
+connection = None
+cursor = None
+conn = sqlite3.connect('./a2.db')
+
+def Check_info(regis_num):
+    c = conn.cursor()
+    c.execute('''Select b.fname||b.lname, v.make, v.model, v.year, v.color
+                 From births b, vehicles v, registrations r
+                 Where b.regno = ? and b.regno = r.regno
+                 and v.vin = r.vin
+                 ''', (regis_num,))
+    rows = c.fetchall()
+    reg_result = ''
+    for row in rows:
+        for element in row:
+            reg_result = reg_result + str(element) + '|'
+    print(reg_result)
+
+def random_num():
+    c = conn.cursor()
+    while True:
+        ticket_num = random.randint(450, 600)
+        c.execute('Select * from tickets t Where t.tno = ?', (ticket_num,))
+        check_num = c.fetchall()
+        if (len(check_num)) != 0:
+            ticket_num = ticket_num + 1
+        else:
+            return ticket_num
+        
+def Issue_ticket(regis_num):
+    c = conn.cursor()
+    v_date = input('Provide the tickets date:')
+    if v_date == 'null':
+        c.execute("Select date('now');")
+        date = c.fetchall()
+        v_date = date[0][0]
+        print(v_date)
+    v_fine = int(input('Provide the tickets fine:'))
+    v_violation = input('Provide the violation text:')
+    ticket_num = random_num()
+    ticket_info = (ticket_num, regis_num, v_fine, v_violation, v_date)
+    c.execute('''Insert into tickets Values(?,?,?,?,?)''', (ticket_info))
+    conn.commit()
+
+def main():
+    regis_num = input('Provide the registration number:')
+    regis_num = int(regis_num)    
+    Check_info(regis_num)
+    Issue_ticket(regis_num)
+    
+main()
