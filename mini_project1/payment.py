e4d187b69b322c4ee1cda6e53563d3389c7b6ad4
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 13:19:19 -0700

    Update payment.py

--- mini_project1/payment.py
+++ mini_project1/payment.py
@@ -5,15 +5,6 @@ from datetime import datetime
 connection = None
 cursor = None
 
-def connect(path):
-    global connection, cursor
-
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    cursor.execute(' PRAGMA forteign_keys=ON; ')
-    connection.commit()
-    return
-
 def payment():
     t_tno = input("Please input ticket number: ")
     t_payment = input("Please input payment: ")
@@ -24,16 +15,16 @@ def payment():
     get_data = cursor.fetchall()   
     if get_data is None:
         raise Exception('Invalid ticket number.')
-    if  t_paymen > get_data[0][2]:
+    if  t_payment > get_data[0][2]:
         raise Exception('Invalid payment.')
     cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
     connection.commit()   
     get_pay = cursor.fetchall()
-    sum_pay = get_pay[0][2] + data[1]
+    sum_pay = get_pay[0][2] +  t_payment
     if sum_pay > get_data[0][2]:
         raise Exception('Invalid payment.')
     pdate = datetime.date(datetime.now())
-    payments = (t_tno, pdate,  t_paymen)
+    payments = (t_tno, pdate,  t_payment)
     cursor.execute('insert into payments values(?,?,?);',payments)
    
     connection.commit()     
