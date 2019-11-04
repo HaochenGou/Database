e6dd3be5c84006e18c7ed9d1fac7e553a5cd9c2e
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 14:23:18 -0700

    Update payment.py

--- mini_project1/payment.py
+++ mini_project1/payment.py
@@ -6,22 +6,24 @@ connection = None
 cursor = None
 
 def payment():
-    t_tno = input("Please input ticket number: ")
+    get_data = 0
+    while get_data == 0:
+        t_tno = input("Please input ticket number: ")
+        get_data = self.cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno}).fetchall()  
+        self.conn.commit() 
+        if get_data == 0 :
+            print("invaild ticket number.")
     t_payment = 0
-    while
-    t_payment = input("Please input payment: ")
-    if  t_payment  == 0:
-        raise Exception('Invalid payment.')
-    self.cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno})
-    self.conn.commit() 
-    get_data = self.cursor.fetchall()   
-    if get_data is None:
-        raise Exception('Invalid ticket number.')
-    if  t_payment > get_data[0][2]:
-        raise Exception('Invalid payment.')
-    self.cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
-    self.conn.commit()   
-    get_pay = self.cursor.fetchall()
+    while t_payment == 0:
+        t_payment = input("Please input payment: ")
+        if  t_payment  == 0:
+            print('Invalid payment.')       
+          
+    while t_payment > get_data[0][2]:
+        print('Invalid payment.')
+        t_payment = input("Please input payment: ")
+        
+    get_pay = self.cursor.execute('select * from payments where tno =:tno', {"tno": t_tno}).fetchall()
     sum_pay = get_pay[0][2] +  t_payment
     if sum_pay > get_data[0][2]:
         raise Exception('Invalid payment.')
