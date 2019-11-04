7ab4b384670f6f9e49867390a728dff7d42e8503
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 13:25:27 -0700

    Update new 01-03.py

--- mini_project1/new 01-03.py
+++ mini_project1/new 01-03.py
@@ -314,6 +314,31 @@ class Interface:
 	    self.cursor.execute('insert into registrations values(?,?,?,?,?,?);',new_data)
 	    self.conn.commit()
 	    return
+	
+	def payment(self):
+	    t_tno = input("Please input ticket number: ")
+	    t_payment = input("Please input payment: ")
+	    if  t_payment  == 0:
+		raise Exception('Invalid payment.')
+	    self.cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno})
+	    self.conn.commit() 
+	    get_data = cursor.fetchall()   
+	    if get_data is None:
+		raise Exception('Invalid ticket number.')
+	    if  t_payment > get_data[0][2]:
+		raise Exception('Invalid payment.')
+	    self.cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
+	    self.conn.commit()   
+	    get_pay = cursor.fetchall()
+	    sum_pay = get_pay[0][2] +  t_payment
+	    if sum_pay > get_data[0][2]:
+		raise Exception('Invalid payment.')
+	    pdate = datetime.date(datetime.now())
+	    payments = (t_tno, pdate,  t_payment)
+	    self.cursor.execute('insert into payments values(?,?,?);',payments)
+
+	    self.conn.commit()    
+	    return
 
 
 main()
