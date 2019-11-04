9c597e86477db72edf2c97e3bec3da962fc39ffc
Zifeiyu <44186320+Zifeiyu2000@users.noreply.github.com>
2019-11-04 14:58:57 -0700

    Update 1-3IDE.py

--- mini_project1/1-3IDE.py
+++ mini_project1/1-3IDE.py
@@ -421,4 +421,125 @@ class Interface:
 				if choice = "r":
 					self.agent_oper()
 
+	def Check_info(self, regis_num):
+		self.cursor.execute('''Select b.fname||b.lname, v.make, v.model, v.year, v.color
+                 From births b, vehicles v, registrations r
+                 Where b.regno = ? and b.regno = r.regno
+                 and v.vin = r.vin
+                 ''', (regis_num,))
+		rows = self.cursor.fetchall()
+		reg_result = ''
+		for row in rows:
+			for element in row:
+				reg_result = reg_result + str(element) + '|'
+		print(reg_result)
+	    
+	def random_num(self):
+		while True:
+			ticket_num = random.randint(450, 600)
+			self.cursor.execute('Select * from tickets t Where t.tno = ?', (ticket_num,))
+			check_num = self.cursor.fetchall()
+			if (len(check_num)) != 0:
+				ticket_num = ticket_num + 1
+			else:
+				return ticket_num
+		    
+	def Issue_ticket(self, regis_num):
+		v_date = input('Provide the tickets date:')
+		if v_date == 'null':
+			self.cursor.execute("Select date('now');")
+			date = self.cursor.fetchall()
+			v_date = date[0][0]
+			print(v_date)
+		v_fine = int(input('Provide the tickets fine:'))
+		v_violation = input('Provide the violation text:')
+		ticket_num = random_num()
+		ticket_info = (ticket_num, regis_num, v_fine, v_violation, v_date)
+		self.cursor.execute('''Insert into tickets Values(?,?,?,?,?)''', (ticket_info))
+		self.conn.commit()
+	    
+	def Question8(self):
+		while True:
+			try:   
+				regis_num = input('Provide the registration number:')
+				regis_num = int(regis_num)
+			except:
+				print('Registration number is integer!')
+				self.cursor.execute("select * from registrations where regno = ?",(regis_num,))
+				finding = self.cursor.fetchall()
+				if len(finding) == 0:
+					print('Wrong registration number!')
+					self.Question8()
+					self.Check_info(regis_num)
+					self.Issue_ticket(regis_num)
+					quit_string = input('Would you like to return to the menu(Y/N):')
+					if quit_string.upper() == 'Y':
+						print('Returning to the menu...')
+						break
+					if quit_string.upper() == 'N':
+						pass
+					else:
+						print('Wrong input!')
+						
+						
+	def Null_check(self, car_make, car_model, car_color, car_year, car_plate, part_sql):
+		Full_sql = "Select distinct v.make, v.model, v.color, v.year, r.plate From registrations r, vehicles v Where r.vin = v.vin "
+		if car_make != 'null':
+			part_sql = part_sql + 'and v.make = ' + '"' + str(car_make) + '"'
+		if car_model != 'null':
+			part_sql = part_sql + 'and v.model = ' + '"' + str(car_model) + '"'
+		if car_color != 'null':
+			part_sql = part_sql + 'and v.color = ' + '"' + str(car_color) + '"'
+		if car_year != 'null':
+			part_sql = part_sql + 'and v.year = ' + '"' + str(car_year) + '"'
+		if car_plate != 'null':
+			part_sql = part_sql + 'and r.plate = ' + '"' + str(car_plate) + '"'
+		Full_sql = Full_sql + part_sql + ";"
+		return Full_sql, part_sql
+		
+	def search_info(part_sql):
+		while True:
+			car_make = input('Provide the make of car:')
+			car_model = input('Provide the model of car:')
+			car_color = input('Provide the color of car:')
+			car_year = input('Provide the year of car:')
+			car_plate = input('Provide the plate of car:')
+			Full_sql, part_sql = self.Null_check(car_make, car_model, car_color, car_year, car_plate, part_sql)
+			self.cursor.execute(Full_sql)
+			rows = self.cursor.fetchall()
+			if len(rows) == 0:
+				print('Can not find the information!')
+				break
+			self.Results(rows, part_sql, part_sql_2)
+		    
+	def Results(self, rows, part_sql, part_sql_2):
+		full_sql_2 = 'Select distinct v.make, v.model, v.color, v.year, r.plate, r.regdate, r.expiry, r.fname||r.lname From registrations r, vehicles v Where r.vin = v.vin ' 
+	    
+		if len(rows) >= 4:
+			for row in rows:
+				print(row)
+			select_list.append(info)
+			Full_sql, part_sql_2 = Null_check(select_list[0], select_list[1], select_list[2], select_list[3], select_list[4], part_sql_2)
+			full_sql_2 = full_sql_2 + part_sql_2
+			self.cursor.execute(full_sql_2)
+			result = self.cursor.fetchall()
+			print(result)
+		
+		if len(rows) < 4 and len(rows) > 0:
+			full_sql_2 = full_sql_2 + part_sql
+			self.cursor.execute(full_sql_2)
+			result = self.cursor.fetchall()
+			print(result)
+		
+	def Question9():
+		while True:
+			search_info(part_sql)
+			quit_string = input('Whould yuu like to return to the menu?(Y/N):')
+			if quit_string.upper() == 'Y':
+				break
+			if quit_string.upper() == 'N':
+				pass
+			else:
+				print('Wrong Input!')
+
 main()
