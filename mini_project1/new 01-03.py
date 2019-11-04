73da74b33660b38792237d2b25bdb1fccf71c9b7
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 13:32:43 -0700

    Update new 01-03.py

--- mini_project1/new 01-03.py
+++ mini_project1/new 01-03.py
@@ -32,11 +32,28 @@ class Interface:
 		print('\n''Exited')
 
 	def login(self):
+import sys
+import hashlib
+import re
 		while self.app_continue and self.valid_login = False:
 			print('\n''Please enter the valid user id and password to login.')
-			uid = input('User Id: ')
-			pwd = imput('Password: ')
-			#password 
+			username = input("Please input uid: ")
+	password = input("Please input password: ")
+	if re.match("^[A-Za-z0-9_]*$", username) and re.match("^[A-Za-z0-9_]*$", password):
+	    encrypt(password)
+	    conn.create_function("hash", 1, encrypt)
+	    data = (username, password)
+	    c.execute(" INSERT INTO users (uid, pwd) VALUES (?, hash(?)) ", data )
+	    data = (password, )
+	    uid = c.execute(" SELECT uid FROM member WHERE pedd LIKE hash(?) ", data).fetchall()
+	    user_uid = c.execute('SELECT * FROM users WHERE uid=?;' , (uid,)).fetchall()
+	    if username != user_uid:
+		raise Exception('Invalid uid or password.')
+		
+	def encrypt(password):
+	    alg = hashlib.sha256()
+	    alg.update(password.encode("utf-8"))
+	    return alg.hexdigest()
 
 	def exit(self):
 		pass
