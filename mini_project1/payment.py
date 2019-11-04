83a5bb3662c3014c8e042f14ddb0c3c5698585ad
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 13:18:12 -0700

    Update payment.py

--- mini_project1/payment.py
+++ mini_project1/payment.py
@@ -14,16 +14,17 @@ def connect(path):
     connection.commit()
     return
 
-def payment(data):
-    t_tno = data[0]
-    if data[1] == 0:
+def payment():
+    t_tno = input("Please input ticket number: ")
+    t_payment = input("Please input payment: ")
+    if  t_payment  == 0:
         raise Exception('Invalid payment.')
     cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno})
     connection.commit() 
     get_data = cursor.fetchall()   
     if get_data is None:
         raise Exception('Invalid ticket number.')
-    if data[0] > get_data[0][2]:
+    if  t_paymen > get_data[0][2]:
         raise Exception('Invalid payment.')
     cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
     connection.commit()   
@@ -32,7 +33,7 @@ def payment(data):
     if sum_pay > get_data[0][2]:
         raise Exception('Invalid payment.')
     pdate = datetime.date(datetime.now())
-    payments = (t_tno, pdate, data[1])
+    payments = (t_tno, pdate,  t_paymen)
     cursor.execute('insert into payments values(?,?,?);',payments)
    
     connection.commit()     
