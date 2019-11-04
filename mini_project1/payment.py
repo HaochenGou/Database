3115cca24324ee12ca6bbb2cc9f8f5f5c0b05cff
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-04 13:56:44 -0700

    Update payment.py

--- mini_project1/payment.py
+++ mini_project1/payment.py
@@ -7,25 +7,27 @@ cursor = None
 
 def payment():
     t_tno = input("Please input ticket number: ")
+    t_payment = 0
+    while
     t_payment = input("Please input payment: ")
     if  t_payment  == 0:
         raise Exception('Invalid payment.')
-    cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno})
-    connection.commit() 
-    get_data = cursor.fetchall()   
+    self.cursor.execute('select * from tickets where tno =:tno limit 1', {"tno": t_tno})
+    self.conn.commit() 
+    get_data = self.cursor.fetchall()   
     if get_data is None:
         raise Exception('Invalid ticket number.')
     if  t_payment > get_data[0][2]:
         raise Exception('Invalid payment.')
-    cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
-    connection.commit()   
-    get_pay = cursor.fetchall()
+    self.cursor.execute('select * from payments where tno =:tno', {"tno": t_tno})
+    self.conn.commit()   
+    get_pay = self.cursor.fetchall()
     sum_pay = get_pay[0][2] +  t_payment
     if sum_pay > get_data[0][2]:
         raise Exception('Invalid payment.')
     pdate = datetime.date(datetime.now())
     payments = (t_tno, pdate,  t_payment)
-    cursor.execute('insert into payments values(?,?,?);',payments)
+    self.cursor.execute('insert into payments values(?,?,?);',payments)
    
-    connection.commit()     
+    self.conn.commit()     
     return
