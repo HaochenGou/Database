2be3efa9944329328363c865595ef53f71ee7fc5
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2019-11-23 20:48:26 -0700

    Add files via upload

---
+++ Mini_group_2/recs.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+from xml.sax.saxutils import escape
+file_name =  input("Please in input correct file name: ")
+tree = ET.parse(file_name)
+root = tree.getroot()
+body = ''
+for h in root:
+    for i in h:
+        row = h[0].text
+        start = '<' + str(i.tag) + '>'
+        if i.text == None:
+            i.text = ''
+        result = escape(str(i.text),{"&":"&amp;","\n":"&#10;","<":"&lt;",">":"&gt;","'":"&apos;",'"': "&quot;"})
+        end = '</' + str(i.tag) + '>'
+        body = body + str(start + result + end)
+    full_information = str(row)+':'+'<mail>'+body+ '</mail>'
+    file = open('recs.txt','a')
+    file.write(full_information)
+    body = ''
+    file.write("\n")
+    file.close()       
+
